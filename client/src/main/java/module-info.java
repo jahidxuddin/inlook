3c787c504d6cc53e62f1de6734925784b629dc12
Jahid Uddin <jahidudd65@gmail.com>
2024-11-08 09:18:20 +0100

    [folder structure]: client & server separated

---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module de.ju.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens de.ju.client to javafx.fxml;
+    exports de.ju.client;
+}
