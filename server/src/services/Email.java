1f8c05e8105e1e3b59a0d3675b9ae35401097069
thisasta <abishan.aranke@outlook.de>
2024-11-13 08:52:44 +0100

    server added

--- server/src/services/Email.java
+++ server/src/services/Email.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Email {
+    private String sender;
+    private String recipient;
+    private String body;
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "From: " + sender + "\nTo: " + recipient + "\nBody:\n" + body;
+    }
 }
--- server/src/services/ServerSMTP.java
+++ server/src/services/ServerSMTP.java
@@ -1,4 +1,100 @@
-package PACKAGE_NAME;
+import socketio.ServerSocket;
+import socketio.Socket;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 public class ServerSMTP {
+    private int port = 1234;
+    private static final List<Email> emails = new ArrayList<Email>();
+    private ServerSocket sSocket;
+    private Socket cSocket;
+    private final String name = "smtp.server.com";
+    private final String expectedUser = "testuser";
+    private final String expectedPassword = "testpassword";
+    private boolean authenticated = false;
+
+    public ServerSMTP() {
+        try {
+            sSocket = new ServerSocket(port);
+            cSocket = sSocket.accept();
+            cSocket.write("220 smtp.example.com ESMTP Service Ready");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void process() {
+        while (true) {
+            try {
+                greeting();
+                if (!authenticated) {
+                    auth();
+                }
+                mailCreation();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void greeting() throws IOException{
+        String temp;
+        while (cSocket.dataAvailable() <= 0);
+        temp = cSocket.readLine();
+        System.out.println("C: " + temp);
+        if (temp.startsWith("EHLO") || temp.startsWith("HELO")) {
+            cSocket.write("250-" + name + " Hello " + temp.substring(5) + "\n");
+        }
+    }
+
+    public void auth() throws IOException{
+        String temp;
+        while (cSocket.dataAvailable() <= 0);
+        temp = cSocket.readLine();
+        if (temp.startsWith("AUTH LOGIN")) {
+            cSocket.write("334 VXNlcm5hbWU6\n");
+            String tempUser = new String(Base64.getDecoder().decode(cSocket.readLine()));
+            cSocket.write("334 UGFzc3dvcmQ6\n");
+            String tempPassword = new String(Base64.getDecoder().decode(cSocket.readLine()));
+            if (tempUser.equalsIgnoreCase(expectedUser) && tempPassword.equals(expectedPassword)) {
+                cSocket.write("235 Authentication successful\n");
+                authenticated = true;
+            } else {
+                cSocket.write("Authentication failed\n");
+            }
+        }
+    }
+
+    public void mailCreation() throws IOException{
+        Email tempEmail = new Email();
+        String temp;
+        while (cSocket.dataAvailable() <= 0);
+        while (true) {
+            temp = cSocket.readLine();
+            if (authenticated && temp.startsWith("MAIL FROM")) {
+                tempEmail.setSender(temp.substring(10));
+                cSocket.write("250 OK\n");
+            } else if (authenticated && temp.startsWith("RCPT TO")) {
+                tempEmail.setRecipient(temp.substring(8));
+                cSocket.write("250 OK\n");
+            } else if (authenticated && temp.startsWith("DATA")) {
+                tempEmail.setBody(temp.substring(5));
+                emails.add(tempEmail);
+                cSocket.write("250 OK: Message Accepted\n");
+            } else if (temp.equals("QUIT")) {
+                cSocket.write("221 Bye\n");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerSMTP serverSMTP = new ServerSMTP();
+        serverSMTP.process();
+    }
 }
+
